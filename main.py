4269a790f8e3ae21bf316ac99c588803fb80c3c7
Bryan <bryan.ferrando@epitech.eu>
2023-10-30 13:40:20 +0000

    Added auto create repository

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import os
+from GithubApi import GithubApi
 from GitFolders import GitFolders
+from dotenv import load_dotenv
 
 def __main__() -> None:
+    load_dotenv()
     gitFolders = GitFolders('/home/username')
+    api = GithubApi(os.getenv('TOKEN'), os.getenv('USERNAME'))
+    for gitFolder in gitFolders.getGitFolders():
+        if not api.checkIfRepositoryExists(gitFolder['name']):
+            api.createNewRepository(gitFolder['name'])
+
+
+
     print(gitFolders.getGitFolders())
 
 
