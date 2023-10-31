e4db9c270334fb19b542ab7599f1f7dd1aaaaf72
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-31 13:53:27 +0100

    change inputs

--- Inputs.py
+++ Inputs.py
@@ -1,56 +1,62 @@
 import os
+from Colors import Colors  # Assuming Colors module exists with color definitions
 
-import Colors as Color
 
-
-def printr(message, color=Color.Colors.UNDERLINE):
-    print(color + message + color)
+def printr(message, color=Colors.UNDERLINE):
+    print(color + message + Colors.ENDC)  # Use ENDC to reset terminal color
 
 
 def get_github_token():
     token = os.getenv('GITHUB_API_TOKEN')
     if not token:
-        token = input("Enter your Github token: ")
+        printr("Please enter your Github token: ", Colors.BOLD)
+        token = input()
     while not token:
-        printr("Github token is required.", Color.Colors.FAIL)
-        token = input("Enter your Github token: ")
+        printr("Github token is required.", Colors.FAIL)
+        printr("Please enter your Github token: ", Colors.BOLD)
+        token = input()
     return token
 
 
 def get_github_username():
     username = os.getenv('GITHUB_USERNAME')
     if not username:
-        username = input("Enter your Github username: ")
+        printr("Please enter your Github username: ", Colors.BOLD)
+        username = input()
     while not username:
-        printr("Github username is required.", Color.Colors.FAIL)
-        username = input("Enter your Github username: ")
+        printr("Github username is required.", Colors.FAIL)
+        printr("Please enter your Github username: ", Colors.BOLD)
+        username = input()
     return username
 
 
 def get_organization_name():
     organization_name = input("Enter organization name: ")
     if not organization_name:
-        printr("Organization name is required.", Color.Colors.FAIL)
-        organization_name = input("Enter organization name: ")
+        printr("Organization name is required.", Colors.FAIL)
+        printr("Please enter the organization name: ", Colors.BOLD)
+        organization_name = input()
     return organization_name
 
 
 def get_folder_path():
     folder_path = input("Enter folder path [ /home/username ]: ")
     if not folder_path:
-        printr("Folder path is required.", Color.Colors.FAIL)
-        folder_path = input("Enter folder path [ /home/username ]: ")
+        printr("Folder path is required.", Colors.FAIL)
+        printr("Please enter the folder path [ /home/username ]: ", Colors.BOLD)
+        folder_path = input()
     return folder_path
 
 
 def get_mode():
-    mode = input(
-        "Choose mode: \n1. Create new repositories from folders \n2. Clone repositories and create them from Github "
-        "URL\n")
-    # Add verification for the mode selected (1 or 2)
+    printr("Choose mode: ", Colors.BOLD)
+    print("1. Create new repositories from folders")
+    print("2. Clone repositories and create them from Github URL")
+    mode = input()
     while mode not in ['1', '2']:
-        printr("Invalid mode. Please choose 1 or 2.", Color.Colors.FAIL)
-        mode = input(
-            "Choose mode: \n1. Create new repositories from folders \n2. Clone repositories and create them from "
-            "Github URL\n")
+        printr("Invalid mode. Please choose 1 or 2.", Colors.FAIL)
+        printr("Choose mode: ", Colors.BOLD)
+        print("1. Create new repositories from folders")
+        print("2. Clone repositories and create them from Github URL")
+        mode = input()
     return mode
